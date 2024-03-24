8ff2312005b5ce918f95fe6f6368f4ac4d258bb1
Neel Shrimali <neelshrimali2@hotmail.com>
2024-03-24 13:05:16 +0530

    main page bug correction

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -4,32 +4,55 @@ import { Link } from "react-router-dom";
 const main = () => {
   return (
     <>
-      {/* header section start */}
-      <div className="header_section">
-        <nav className="destop_header navbar navbar-expand-lg navbar-light bg-light">
-          <div className="logo"></div>
-          <div className="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul className="navbar-nav mr-auto">
-              <li className="nav-item">
-                <a className="logo_main" href="index.html">
-                  <img src="images/logo.png" alt="Logo" />
-                </a>
+      {/* <!-- header section start --> */}
+      <div class="header_section">
+        <nav class="destop_header navbar navbar-expand-lg navbar-light bg-light">
+          <div class="logo"></div>
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav mr-auto">
+              <li class="nav-item">
+                <a class="nav-link" href="index.html">Home</a>
               </li>
-              <li className="nav-item">
-                <Link to="/" className="nav-link">
-                  Home
-                </Link>
+              <li class="nav-item">
+                <a class="nav-link" href="about.html">About</a>
               </li>
-              <li className="nav-item">
-                <Link to="/" className="nav-link">
-                  About
-                </Link>
+              <li class="nav-item">
+                <a class="nav-link" href="doctor.html">Doctor</a>
               </li>
-              <li className="nav-item">
-                <Link to="/" className="nav-link">
-                  Contact
-                </Link>
+              <li class="nav-item">
+                <a class="logo_main" href="index.html"><img src="images/logo.png"/></a>
               </li>
+              <li class="nav-item">
+                <a class="nav-link" href="blog.html">Blog</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="contact.html">Contact</a>
+              </li>                       
+            </ul>
+          </div>
+        </nav>
+        <nav class="mobile_header navbar navbar-expand-lg navbar-light bg-light">
+          <div class="logo"><a href="index.html"><img src="images/logo.png"/></a></div>
+          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent2" aria-controls="navbarSupportedContent2" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarSupportedContent2">
+            <ul class="navbar-nav mr-auto">
+              <li class="nav-item">
+                <a class="nav-link" href="index.html">Home</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="about.html">About</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="doctor.html">Doctor</a>
+              </li>                            
+              <li class="nav-item">
+                <a class="nav-link" href="blog.html">Blog</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="contact.html">Contact</a>
+              </li>                       
             </ul>
           </div>
         </nav>
